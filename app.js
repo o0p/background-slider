c8133f2fb997e24e9ab789bfdcd82b01d5bed1b9
Russell <russelldev@protonmail.com>
2021-01-13 09:57:23 +0000

    Add right button functionality

--- app.js
+++ app.js
@@ -4,13 +4,23 @@ const leftBtn = document.getElementById('left');
 const rightBtn = document.getElementById('right');
 let activeSlide = 0;
 
-setBgImage();
+rightBtn.addEventListener('click', function(){
+    activeSlide = activeSlide + 1;
 
-function setBgImage() {
+    if(activeSlide > slides.length - 1) {
+         activeSlide = 0;
+        };
+                
+    setBgToBody();
+    setActiveSlide();
+});
+
+setBgToBody();
+
+function setBgToBody() {
     body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
 };
-
 function setActiveSlide() {
     slides.forEach((slide) => slide.classList.remove('active'));
     slides[activeSlide].classList.add('active');
-}
+};

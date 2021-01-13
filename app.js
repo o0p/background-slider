c6f4e7d71e8cbc170bc32a861e2b49b1980951a8
Russell <russelldev@protonmail.com>
2021-01-13 09:24:09 +0000

    Add slide funct to cast on bg

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ const slides = document.querySelectorAll('.slide');
 const leftBtn = document.getElementById('left');
 const rightBtn = document.getElementById('right');
 let activeSlide = 0;
+
+setBgImage();
+
+function setBgImage() {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+};
